dc05907f08650a94070837bea4fe37cf47389372
Benjamin Vaughn <benjaminvaughn91@gmail.com>
2021-08-30 21:06:26 +0200

    test

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 3001
 const MONGODB_URI = process.env.MONGODB_URI
-const TMDB_KEY = process.env.TMDB_KEY || '91e4e06b1420960f3271a94241456a7f'
-const TMDB_IMG_URL = process.env.TMDB_IMG_URL || 'https://image.tmdb.org/t/p/w500'
-const TMDB_BASE_URL = process.env.TMDB_BASE_URL || 'https://api.themoviedb.org/3'
+const TMDB_KEY = process.env.TMDB_KEY
+const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/w500'
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
 
 module.exports = {
   MONGODB_URI,
