3cfde95e3570e86ad620d9e591c02df00f5e1b4f
Benjamin Vaughn <benjaminvaughn91@gmail.com>
2021-09-02 17:02:39 +0200

    added base url

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3001
 const MONGODB_URI = process.env.MONGODB_URI
 const TMDB_KEY = process.env.TMDB_KEY
-const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/w500'
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
+const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/w500'
 
 module.exports = {
   MONGODB_URI,
